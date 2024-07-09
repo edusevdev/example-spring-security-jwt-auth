30b6d9f808dca6b701fba2b4449bfebd22583d3d
edusevdev <edusevdev@gmail.com>
2024-07-09 16:48:29 +0200

    CORS configuration

--- src/main/java/com/example/authjwt/ws/controller/TestController.java
+++ src/main/java/com/example/authjwt/ws/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.example.authjwt.ws.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://www.serverclient.com:3000"}) // It's necessary for being used from the other server
 public class TestController {
 
     @PreAuthorize("hasRole('ROLE_admin')")
